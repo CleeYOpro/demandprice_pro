4c7a2a34b192ab24704de22934b0b4d597cdf7ac
CleeYOpro <cbalaranjith@gmail.com>
2025-06-15 22:04:52 -0700

    Update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/priceprofitdemand/', // subpath
+  base: '/', // 👈 important
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
